b97215a3f58837733825fa67caa119d4a19b7d8c
Kevin Kim <kevin.kim03@sap.com>
2019-03-08 13:31:20 -0500

    first

---
+++ pdfview-restore.el
@@ -0,0 +1,31 @@
+;;; pdfview-restore.el --- Support for reopening pdf to last position opened
+
+;; Copyright (C) 2019 Kevin Kim
+
+;; Author: Kevin Kim <kevinkim1991@gmail.com>
+;; Maintainer: Kevin Kim <kevinkim1991@gmail.com>
+;; Keywords: pdf-view, pdf-tools
+;; Version: 0.1
+;; Package-Requires: ((pdf-tools "0.80"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(require 'pdf-tools)
+(require 'pdf-view)
+
+(provide 'pdfview-restore)
+;;; pdfview-restore.el ends here
