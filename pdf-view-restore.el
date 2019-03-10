9d092d86c2f9a2d97a3ed65f601efbcdc0597dc1
local <local>
2019-03-10 00:01:06 -0500

    conform to package-lint

--- pdf-view-restore.el
+++ pdf-view-restore.el
@@ -1,25 +1,29 @@
-;;; pdf-view-restore.el --- Support for saving and opening last known pdf position in pdfview mode
+;;; pdf-view-restore.el --- Support for opening last known pdf position in pdfview mode
 
-;; Copyright (C) 2019 Kevin Kim
+;; Copyright (c) 2019 Kevin Kim <kevinkim1991@gmail.com>
 
 ;; Author: Kevin Kim <kevinkim1991@gmail.com>
-;; Maintainer: Kevin Kim <kevinkim1991@gmail.com>
-;; Keywords: pdf-view, pdf-tools
+;; URL: https://github.com/007kevin/pdf-view-restore
+;; Keywords: files convenience
 ;; Version: 0.1
-;; Package-Requires: ((pdf-tools "0.90"))
+;; Package-Requires: ((pdf-tools "0.90") (emacs "24.3"))
 
-;; This program is free software; you can redistribute it and/or modify
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; Support for saving and opening last known pdf position in pdfview mode. Information
@@ -31,23 +35,24 @@
 ;;   (add-hook 'kill-emacs-hook 'pdf-view-restore-save)
 ;;   (add-hook 'pdf-view-mode-hook 'pdf-view-restore)
 
-
 ;;; Code:
+
 (require 'pdf-view)
-(require 'subr-x) ;; For if-let macro
 
 (defcustom pdf-view-restore-filename ".pdf-view-restore"
-  "Filename to save last known pdf position"
+  "Filename to save the last known pdf position."
   :group 'pdf-view-restore
   :type 'string)
 
+;;;###autoload
 (defun pdf-view-restore ()
   "Restore page"
   (when (eq major-mode 'pdf-view-mode)
     ;; This buffer is in pdf-view-mode
-    (pdf-view-goto-page (pdf-view-restore-get-page))))
+    (let ((page (pdf-view-restore-get-page)))
+      (if page (pdf-view-goto-page page)))))
 
-
+;;;###autoload
 (defun pdf-view-restore-save ()
   "Save restore information"
   (when (eq major-mode 'pdf-view-mode)
@@ -58,8 +63,9 @@
 (defun pdf-view-restore-get-page ()
   "Return restore page"
   (let* ((alist (pdf-view-restore-unserialize))
-         (key (pdf-view-restore-key)))
-    (if alist (cdr (assoc key alist)) 0)))
+         (key (pdf-view-restore-key))
+         (val (cadr (assoc key alist))))
+    val))
 
 (defun pdf-view-restore-set-page (page)
   "Save restore page"
@@ -67,21 +73,10 @@
          (key (pdf-view-restore-key)))
     (pdf-view-restore-serialize (pdf-view-restore-alist-set key page alist))))
 
-(defun pdf-view-restore-alist-set (key val alist &optional symbol)
-  "Set property KEY to VAL in ALIST. Return new alist.
-This creates the association if it is missing, and otherwise sets
-the cdr of the first matching association in the list. It does
-not create duplicate associations. By default, key comparison is
-done with `equal'. However, if SYMBOL is non-nil, then `eq' is
-used instead.
-
-This method may mutate the original alist, but you still need to
-use the return value of this method instead of the original
-alist, to ensure correct results."
-  (if-let ((pair (if symbol (assq key alist) (assoc key alist))))
-      (setcdr pair val)
-    (push (cons key val) alist))
-  alist)
+(defun pdf-view-restore-alist-set (key val alist)
+  "Set property KEY to VAL in ALIST. Return new alist."
+  (let ((alist (delq (assoc key alist) alist)))
+    (add-to-list 'alist `(,key ,val))))
 
 (defun pdf-view-restore-key () (file-name-base buffer-file-name))
 
